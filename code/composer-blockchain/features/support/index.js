1088a79ab105cfe4dde119a032a008dd383fe047
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2020-11-28 08:11:24 +0000

    Fix composer.json

--- code/composer-blockchain/features/support/index.js
+++ code/composer-blockchain/features/support/index.js
@@ -17,4 +17,10 @@
 const composerSteps = require('composer-cucumber-steps');
 const cucumber = require('cucumber');
 
-module.exports = functi
+module.exports = function () {
+    composerSteps.call(this);
+};
+
+if (cucumber.defineSupportCode) {
+    cucumber.defineSupportCode((context) => {
+        module.exports.call(con
