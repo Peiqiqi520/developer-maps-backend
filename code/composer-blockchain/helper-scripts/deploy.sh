2e09d611913590e0a06e9a698411ed17e6e3f8f5
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2020-12-05 11:34:09 +0000

    ring-buffer: Fix infinite spin in reading buffer

--- code/composer-blockchain/helper-scripts/deploy.sh
+++ code/composer-blockchain/helper-scripts/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "Be sure to run the 'startFabric.s
+echo "Be sure to run the 'startFabric.sh' script first!"
+composer network install --card PeerAdmin@hlfv1 --archiveFile oracle-asset-track@0.0.8.bna
+#comp
