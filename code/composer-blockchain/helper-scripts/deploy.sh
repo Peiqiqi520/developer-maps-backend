ad8016543793605d7fdd8743dc40cc358aae9597
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2020-12-06 09:26:48 +0000

    PCI / PM: Resume runtime-suspended devices later during system suspend

--- code/composer-blockchain/helper-scripts/deploy.sh
+++ code/composer-blockchain/helper-scripts/deploy.sh
@@ -3,4 +3,4 @@
 echo "Be sure to run the 'startFabric.sh' script first!"
 composer network install --card PeerAdmin@hlfv1 --archiveFile oracle-asset-track@0.0.8.bna
 #composer network upgrade -c PeerAdmin@hlfv1 -n oracle-asset-track -V 0.0.8
-composer network start --networkName oracle-asset-track --net
+composer network start --networkName oracle-asset-track --networkVersion 0.0.8 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --fil
