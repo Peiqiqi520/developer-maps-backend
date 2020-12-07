5306c700d3f946c6195282849297adf367fa020e
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2020-12-07 09:59:31 +0000

    arm: remove "optimized" SHA1 routines

--- code/composer-blockchain/helper-scripts/fabric-scripts/_loader.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/_loader.sh
@@ -2,4 +2,8 @@ echo "Development only script for Hyperledger Fabric control"
 
 
 # Grab the current directory
-DIR=
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+THIS_SCRIPT=`basename "$0"`
+echo "Running '${THIS_SCRIPT}'"
+
+i
