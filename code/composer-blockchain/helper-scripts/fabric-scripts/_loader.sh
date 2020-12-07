d4135b82f3f72efb096ff71028316ac11afebb1c
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2020-12-07 18:46:17 +0000

    iommu/amd: Check for the right TLP prefix bit

--- code/composer-blockchain/helper-scripts/fabric-scripts/_loader.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/_loader.sh
@@ -6,4 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 THIS_SCRIPT=`basename "$0"`
 echo "Running '${THIS_SCRIPT}'"
 
-i
+if [ "${HL_FABRIC_VERSION}" ]; then
+  export FABRIC_VERSION="${HL_FABRIC_VERSION}"
+fi
+
+if [ "${HL_FABRI
