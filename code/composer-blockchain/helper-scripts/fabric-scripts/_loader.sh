81b30a3c9a333dffac879df904ba2219b60744ef
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2020-12-08 20:26:11 +0000

    makefile: update Makefile.common with newer version

--- code/composer-blockchain/helper-scripts/fabric-scripts/_loader.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/_loader.sh
@@ -10,4 +10,8 @@ if [ "${HL_FABRIC_VERSION}" ]; then
   export FABRIC_VERSION="${HL_FABRIC_VERSION}"
 fi
 
-if [ "${HL_FABRI
+if [ "${HL_FABRIC_START_TIMEOUT}" ]; then
+  export FABRIC_START_TIMEOUT="${HL_FABRIC_START_TIMEOUT}"
+fi
+
+if [ -z ${FABRIC_VERSI
