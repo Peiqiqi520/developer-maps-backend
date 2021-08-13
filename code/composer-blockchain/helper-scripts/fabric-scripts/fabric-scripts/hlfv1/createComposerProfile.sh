a9e50f2f6b9866af92b72fefce85a89bd92cbd07
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2021-08-13 19:39:31 +0000

    Update Xcode project

--- code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createComposerProfile.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createComposerProfile.sh
@@ -3,3 +3,7 @@
 # Exit on first error, print all commands.
 set -ev
 # Grab the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+rm -rf ~/.composer-connection-profiles/hlfv1/*
+rm -rf ~/.composer-cr
