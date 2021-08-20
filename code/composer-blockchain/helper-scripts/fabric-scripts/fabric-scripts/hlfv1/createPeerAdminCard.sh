6a716327bbe9ad92bbe8892e187f6a21e8a9801f
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2021-08-20 06:00:26 +0000

    import sys

--- code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Grab the current directory
-DIR="$( cd "$( dirna
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z "${HL_COMPOSER_CLI}" ]; then
+  HL_COMPOSER_CLI=$(which compo
