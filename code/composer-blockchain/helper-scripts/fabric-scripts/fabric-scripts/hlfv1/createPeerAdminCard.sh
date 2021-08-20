ddd7ff439ef0e7803f4acc2087f0ae23e519fafa
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2021-08-20 06:52:11 +0000

    Automated Impex Update

--- code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -4,4 +4,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -z "${HL_COMPOSER_CLI}" ]; then
-  HL_COMPOSER_CLI=$(which compo
+  HL_COMPOSER_CLI=$(which composer)
+fi
+
+echo
+# check that the composer command exists at a version >v0.14
+COMPOSER_VERSION=$("${HL_COMPOSER_
