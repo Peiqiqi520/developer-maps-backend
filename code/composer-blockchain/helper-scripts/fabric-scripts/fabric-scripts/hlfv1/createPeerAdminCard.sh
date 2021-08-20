752debd5e5b63395139735b1f82a2924dd9736c1
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2021-08-20 12:03:16 +0000

    Standardize the rubocop file

--- code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -13,4 +13,6 @@ COMPOSER_VERSION=$("${HL_COMPOSER_CLI}" --version 2>/dev/null)
 COMPOSER_RC=$?
 
 if [ $COMPOSER_RC -eq 0 ]; then
-    AWKRET=$(echo $COMPOSER_VERSION | awk -F. '{if ($2<1
+    AWKRET=$(echo $COMPOSER_VERSION | awk -F. '{if ($2<15 || $2>16) print "1"; else print "0";}')
+    if [ $AWKRET -eq 1 ]; then
+        echo $COMPOSER_VERSION is not supported
