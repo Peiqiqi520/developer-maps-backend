8261d90bfc27b5645b95595ac071ed46867b2c34
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2021-08-22 05:57:21 +0000

    Fix scripts

--- code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -15,4 +15,7 @@ COMPOSER_RC=$?
 if [ $COMPOSER_RC -eq 0 ]; then
     AWKRET=$(echo $COMPOSER_VERSION | awk -F. '{if ($2<15 || $2>16) print "1"; else print "0";}')
     if [ $AWKRET -eq 1 ]; then
-        echo $COMPOSER_VERSION is not supported
+        echo $COMPOSER_VERSION is not supported for this level of fabric. Please use version 0.16
+        exit 1
+    else
+        echo Using composer-cli at $COMPOSER_VE
