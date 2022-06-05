d4248c45aeb56463b1f0148debfe350121b5327f
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2022-06-05 20:56:41 +0000

    rename config

--- code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv11/downloadFabric.sh
+++ code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv11/downloadFabric.sh
@@ -12,4 +12,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Pull and tag the latest Hyperledger Fabric base image.
 docker pull hyperledger/fabric-peer:$ARCH-1.1.0
 docker pull hyperledger/fabric-ca:$ARCH-1.1.0
-docker pull hyperledger/fabric-ccenv:$ARCH-
+docker pull hyperledger/fabric-ccenv:$ARCH-1.1.0
+docker pull hyperledger/fabric-orderer:$ARCH-1.1.0
+docker pull hyperledger/fabric-couchdb:$ARCH-0.4.6
---
+++ code/composer-blockchain/helper-scripts/fabric-scripts/fabric-scripts/hlfv11/startFabric.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -e
+
+Usage() {
+	echo ""
+	echo "Usage: ./startFabric.sh [-d || --dev]"
+	echo ""
