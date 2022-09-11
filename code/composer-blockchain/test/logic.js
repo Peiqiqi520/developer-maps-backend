ea8f653fbad47602acd07811c128d5e585e92af4
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2022-09-11 12:32:18 +0000

    Create .empty

--- code/composer-blockchain/test/logic.js
+++ code/composer-blockchain/test/logic.js
@@ -58,3 +58,137 @@ describe('#' + namespace, () => {
     // These are the identities for Alice and Bob.
     const aliceCardName = 'alice';
     const bobCardName = 'bob';
+
+    // These are a list of receieved events.
+    let events;
+
+    let businessNetworkName;
+
+    before(async () => {
+        // Generate certificates for use with the embedded connection
+        const credentials = CertificateUtil.generate({ commonName: 'admin' });
+
+        // Identity used with the admin connection to deploy business networks
+        const deployerMetadata = {
+            version: 1,
+            userName: 'PeerAdmin',
+            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
+        };
+        const deployerCard = new IdCard(deployerMetadata, connectionProfile);
+        deployerCard.setCredentials(credentials);
+        const deployerCardName = 'PeerAdmin';
+
+        adminConnection = new AdminConnection({ cardStore: cardStore });
+
+        await adminConnection.importCard(deployerCardName, deployerCard);
+        await adminConnection.connect(deployerCardName);
+    });
+
+    /**
+     *
+     * @param {String} cardName The card name to use for this identity
+     * @param {Object} identity The identity details
+     */
+    async function importCardForIdentity(cardName, identity) {
+        const metadata = {
+            userName: identity.userID,
+            version: 1,
+            enrollmentSecret: identity.userSecret,
+            businessNetwork: businessNetworkName
+        };
+        const card = new IdCard(metadata, connectionProfile);
+        await adminConnection.importCard(cardName, card);
+    }
+
+    // This is called before each test is executed.
+    beforeEach(async () => {
+        // Generate a business network definition from the project directory.
+        let businessNetworkDefinition = await BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+        businessNetworkName = businessNetworkDefinition.getName();
+        await adminConnection.install(businessNetworkDefinition);
+        const startOptions = {
+            networkAdmins: [
+                {
+                    userName: 'admin',
+                    enrollmentSecret: 'adminpw'
+                }
+            ]
+        };
+        const adminCards = await adminConnection.start(businessNetworkName, businessNetworkDefinition.getVersion(), startOptions);
+        await adminConnection.importCard(adminCardName, adminCards.get('admin'));
+
+        // Create and establish a business network connection
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+        events = [];
+        businessNetworkConnection.on('event', event => {
+            events.push(event);
+        });
+        await businessNetworkConnection.connect(adminCardName);
+
+        // Get the factory for the business network.
+        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+        const participantRegistry = await businessNetworkConnection.getParticipantRegistry(participantNS);
+        // Create the participants.
+        const alice = factory.newResource(namespace, participantType, 'alice@email.com');
+        alice.firstName = 'Alice';
+        alice.lastName = 'A';
+
+        const bob = factory.newResource(namespace, participantType, 'bob@email.com');
+        bob.firstName = 'Bob';
+        bob.lastName = 'B';
+
+        participantRegistry.addAll([alice, bob]);
+
+        const assetRegistry = await businessNetworkConnection.getAssetRegistry(assetNS);
+        // Create the assets.
+        const asset1 = factory.newResource(namespace, assetType, '1');
+        asset1.owner = factory.newRelationship(namespace, participantType, 'alice@email.com');
+        asset1.value = '10';
+
+        const asset2 = factory.newResource(namespace, assetType, '2');
+        asset2.owner = factory.newRelationship(namespace, participantType, 'bob@email.com');
+        asset2.value = '20';
+
+        assetRegistry.addAll([asset1, asset2]);
+
+        // Issue the identities.
+        let identity = await businessNetworkConnection.issueIdentity(participantNS + '#alice@email.com', 'alice1');
+        await importCardForIdentity(aliceCardName, identity);
+        identity = await businessNetworkConnection.issueIdentity(participantNS + '#bob@email.com', 'bob1');
+        await importCardForIdentity(bobCardName, identity);
+    });
+
+    /**
+     * Reconnect using a different identity.
+     * @param {String} cardName The name of the card for the identity to use
+     */
+    async function useIdentity(cardName) {
+        await businessNetworkConnection.disconnect();
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+        events = [];
+        businessNetworkConnection.on('event', (event) => {
+            events.push(event);
+        });
+        await businessNetworkConnection.connect(cardName);
+        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+    }
+
+    it('Alice can read all of the assets', async () => {
+        // Use the identity for Alice.
+        await useIdentity(aliceCardName);
+        const assetRegistry = await businessNetworkConnection.getAssetRegistry(assetNS);
+        const assets = await assetRegistry.getAll();
+
+        // Validate the assets.
+        assets.should.have.lengthOf(2);
+        const asset1 = assets[0];
+        asset1.owner.getFullyQualifiedIdentifier().should.equal(participantNS + '#alice@email.com');
+        asset1.value.should.equal('10');
+        const asset2 = assets[1];
+        asset2.owner.getFullyQualifiedIdentifier().should.equal(participantNS + '#bob@email.com');
+        asset2.value.should.equal('20');
+    });
+
+    it('Bob can read all of the assets', async () => {
+        // Use the identity for Bob.
