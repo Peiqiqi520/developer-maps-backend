e8d2d813427c2773a54fcb9f82914eb188eb2cf0
Peiqiqi520 <93337536+Peiqiqi520@users.noreply.github.com>
2022-09-11 11:31:34 +0000

    Added more test cases

---
+++ code/composer-blockchain/test/logic.js
@@ -0,0 +1,60 @@
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+/**
+ * Write the unit tests for your transction processor functions here
+ */
+
+const AdminConnection = require('composer-admin').AdminConnection;
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
+const path = require('path');
+
+const chai = require('chai');
+chai.should();
+chai.use(require('chai-as-promised'));
+
+const namespace = 'org.example.biznet';
+const assetType = 'SampleAsset';
+const assetNS = namespace + '.' + assetType;
+const participantType = 'SampleParticipant';
+const participantNS = namespace + '.' + participantType;
+
+describe('#' + namespace, () => {
+    // In-memory card store for testing so cards are not persisted to the file system
+    const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
+
+    // Embedded connection used for local testing
+    const connectionProfile = {
+        name: 'embedded',
+        'x-type': 'embedded'
+    };
+
+    // Name of the business network card containing the administrative identity for the business network
+    const adminCardName = 'admin';
+
+    // Admin connection to the blockchain, used to deploy the business network
+    let adminConnection;
+
+    // This is the business network connection the tests will use.
+    let businessNetworkConnection;
+
+    // This is the factory for creating instances of types.
+    let factory;
+
+    // These are the identities for Alice and Bob.
+    const aliceCardName = 'alice';
+    const bobCardName = 'bob';
